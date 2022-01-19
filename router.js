88709a323d03cf488fd1a798a8f14a9d75a9297d
mmyona <hyon2001@ajou.ac.kr>
2022-01-20 02:16:22 +0900

    express 라우터 다시 구현 완료

--- router.js
+++ router.js
@@ -1,38 +1,17 @@
 // template
-const aTemplate = require('./pages/자유.hbs')
-const bTemplate = require('./pages/비밀.hbs')
-const cTemplate = require('./pages/정보.hbs')
-const dTemplate = require('./pages/홍보.hbs')
-const eTemplate = require('./pages/SW.hbs')
-const homeTemplate = require('./pages/home.hbs')
-const loginTemplate = require('./pages/login.hbs')
-const signupTemplate = require('./pages/signup.hbs')
-const fTemplate = require('./pages/write.hbs')
-const gTemplate = require('./pages/read.hbs')
+const chatTemplate = require('./static/pages/chat.hbs')
+const homeTemplate = require('./static/pages/home.hbs')
+const loginTemplate = require('./static/pages/login.hbs')
 
+const chat = chatTemplate()
 const Home = homeTemplate()
-const a= aTemplate()
-const b = bTemplate()
-const c = cTemplate()
-const d = dTemplate()
-const e = eTemplate()
 const login = loginTemplate()
-const signup = signupTemplate()
-const f = fTemplate()
-const g = gTemplate()
 
 const routes = {
   '/': Home,
   '/home': Home,
-  '/자유': a,
-  '/비밀': b,
-  '/정보': c,
-  '/홍보': d,
-  '/SW': e,
   '/login': login,
-  '/signup': signup,
-  '/write': f,
-  '/read' : g
+  '/chat' : chat
 }
 
 //sql 연결 라우터
