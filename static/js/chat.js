547828cd3dea4995b05644892a5c476492278cd6
mmyona <hyon2001@ajou.ac.kr>
2022-01-21 02:21:52 +0900

    아직 hbs에서 img src하는 것 해결 못함

---
+++ static/js/chat.js
@@ -0,0 +1,20 @@
+const chat=require('../pages/chat.hbs');
+const chatBox = chat.querySelector('#chat-box');
+const input = chat.querySelector('#input');
+const send = chat.querySelector('#send');
+
+function sendMyText() {
+  const newMessage = input.value;
+  if (newMessage) {
+    const div = chat.createElement('div');
+    div.classList.add('bubble', 'my-bubble');
+    div.innerText = newMessage;
+    chatBox.append(div);
+  } else {
+    alert('메시지를 입력하세요...');
+  }
+
+  input.value = '';
+}
+
+send.addEventListener('click', sendMyText);
--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,10 @@ module.exports = {
       {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.png/, 
+        loader: 'url-loader'
       }
     ]
   }
